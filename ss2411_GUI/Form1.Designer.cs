d7252ba3d7e63c51c3c9f714b43e9f094e6b8c25
ShomaShirai <22t0837a@student.gs.chiba-u.jp>
2025-01-25 15:15:35 +0900

    プロジェクト ファイルを追加します。

---
+++ ss2411_GUI/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ss2411_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(630, 434);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.label1.Location = new System.Drawing.Point(30, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "開始ボタンを押してください";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 513);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ss2411_GUI/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace ss2411_GUI
+{
+    public partial class Form1 : Form
+    {
+        private bool isReceiving = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 画像受信のためのスレッドを開始
+            Task.Run(() => ReceiveImages());
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            // フォーム終了時に受信を停止
+            isReceiving = false;
+        }
+
+        private void ReceiveImages()
+        {
+            using (var subscriber = new SubscriberSocket())
+            {
+                // サブスクライブ設定
+                subscriber.Connect("tcp://localhost:5556");
+                subscriber.Subscribe("");  // 全てのメッセージを受信
+
+                while (isReceiving)
+                {
+                    try
+                    {
+                        // 画像データを受信
+                        byte[] frameData = subscriber.ReceiveFrameBytes();
+                        using (var ms = new MemoryStream(frameData))
+                        {
+                            // バイトデータを画像に変換
+                            Image image = Image.FromStream(ms);
+
+                            // PictureBoxに画像を表示
+                            pictureBox1.Invoke((MethodInvoker)(() =>
+                            {
+                                pictureBox1.Image = image;
+                            }));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("受信エラー: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
